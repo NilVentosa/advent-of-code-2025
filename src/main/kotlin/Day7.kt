ac11b8673b61bcb67402c30bf4124edc8f880397
nilventosa <ventosa@gmail.com>
2025-12-13 14:54:12 +0100

    empty day 7

---
+++ src/main/kotlin/Day7.kt
@@ -0,0 +1,11 @@
+class Day7 {
+    private val testInput = this::class.java.getResourceAsStream("/day7testinput")?.bufferedReader()?.readText()?.trim() ?: ""
+    private val input = this::class.java.getResourceAsStream("/day7input")?.bufferedReader()?.readText()?.trim() ?: ""
+
+    fun part1() {
+    }
+
+    fun part2() {
+    }
+
+}
